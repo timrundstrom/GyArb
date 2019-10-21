8aa202f4dc0cf37d551bc1c5c4571447e5d542c9
Tim Rundström <timmi.rundstrom@gmail.com>
2019-10-21 18:06:46 +0200

    project

---
+++ Gym.Pro/Assets/Scripts/SimulationSettings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class SimulationSettings : ScriptableObject {
+	// Settings for projectile simulation with 2 dimensions.
+
+	[HideInInspector]
+	public bool useZAxis = false;
+
+	[HideInInspector]
+	public SimValues simValues;
+
+	[HideInInspector]
+	public float verticalAngle;
+	[HideInInspector]
+	public float horizontalAngle;
+	[HideInInspector]
+
+	public float startVelocity;
+	[HideInInspector]
+	public float startVelocityX;
+	[HideInInspector]
+	public float startVelocityY;
+	[HideInInspector]
+	public float startVelocityZ;
+
+	public Vector3 startPosition;
+	public float floor = 0f;
+
+}
