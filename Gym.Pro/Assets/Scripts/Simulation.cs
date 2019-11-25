4216d912afdcb8d9f999d6beef1669b95cb46c42
Tim Rundström <42844873+timrundstrom@users.noreply.github.com>
2019-11-25 12:49:58 +0100

    .

--- Gym.Pro/Assets/Scripts/Simulation.cs
+++ Gym.Pro/Assets/Scripts/Simulation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SimValues { VAlpha, VxAlpha, VyAlpha, VxVy };
 public class Simulation : MonoBehaviour {
 
 	public GameObject projectileObject;
